6a6a58bbc37a6c006ea6ef5b1ffffa0b8d244def
Dave Lewanda <dlewanda@yahoo.com>
2015-07-07 07:01:16 -0400

    Add number exception handling, improve layout

--- app/src/main/java/com/davelewanda/currencyconverter/CurrencyConverterActivity.java
+++ app/src/main/java/com/davelewanda/currencyconverter/CurrencyConverterActivity.java
@@ -1,13 +1,11 @@
 package com.davelewanda.currencyconverter;
 
 import android.app.Activity;
-import android.app.DownloadManager;
+import android.app.AlertDialog;
 import android.content.Context;
-import android.support.v7.app.ActionBarActivity;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.text.Editable;
-import android.text.TextWatcher;
 import android.util.Log;
 import android.view.KeyEvent;
 import android.view.LayoutInflater;
@@ -32,35 +30,20 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 
 
 public class CurrencyConverterActivity extends AppCompatActivity {
 
+    private static RequestQueue requestQueue = null;
+    private final int COUNTRY_COUNT = 4;
     private EditText usDollarsEditText;
     private ListView currencyListView;
-    private final int COUNTRY_COUNT = 4;
     private CurrencyAdapter currencyAdapter;
-    private static RequestQueue requestQueue = null;
-    private class Currency {
-        String country;
-        Double currencyAmount;
-        public Currency(String country, Double currencyAmount) {
-            this.country = country;
-            this.currencyAmount = currencyAmount;
-        }
-    }
-
     private ArrayList<Currency> currencyArrayList = new ArrayList<>();
-
-    public static class ViewHolder {
-        public TextView countryTextView;
-        public TextView currencyTextView;
-    }
 
     public void setCurrencies(final int usd) {
         final Double gbp = 0.0, eur = 0.0, jpy = 0.0, brl = 0.0;
-        if(usd != 0) {
+        if (usd != 0) {
             String url = "http://api.fixer.io/latest?base=USD";
 
             JsonObjectRequest jsObjRequest = new JsonObjectRequest
@@ -72,7 +55,8 @@ public class CurrencyConverterActivity extends AppCompatActivity {
                             try {
                                 JSONObject rates = (JSONObject) response.get("rates");
                                 (currencyArrayList.get(0)).currencyAmount = usd * rates.getDouble("GBP");
-                                (currencyArrayList.get(1)).currencyAmount = usd * rates.getDouble("EUR");;
+                                (currencyArrayList.get(1)).currencyAmount = usd * rates.getDouble("EUR");
+                                ;
                                 (currencyArrayList.get(2)).currencyAmount = usd * rates.getDouble("JPY");
                                 (currencyArrayList.get(3)).currencyAmount = usd * rates.getDouble("BRL");
                                 currencyAdapter.notifyDataSetChanged();
@@ -89,7 +73,7 @@ public class CurrencyConverterActivity extends AppCompatActivity {
 
                         }
                     });
-                requestQueue.add(jsObjRequest);
+            requestQueue.add(jsObjRequest);
 
         } else {
             currencyArrayList.add(new Currency(getString(R.string.GBP), gbp));
@@ -99,68 +83,12 @@ public class CurrencyConverterActivity extends AppCompatActivity {
         }
     }
 
-    private class CurrencyAdapter extends BaseAdapter {
-
-        private final Activity activity;
-        private final LayoutInflater inflater;
-
-        public CurrencyAdapter(Activity a) {
-            this.activity = a;
-            this.inflater = (LayoutInflater)activity.
-                    getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-        }
-        @Override
-        public int getCount() {
-            return COUNTRY_COUNT;
-        }
-
-        @Override
-        public Object getItem(int position) {
-            return position;
-        }
-
-        @Override
-        public long getItemId(int position) {
-            return position;
-        }
-
-        @Override
-        public View getView(int position, View convertView, ViewGroup parent) {
-            View view = convertView;
-            ViewHolder holder;
-
-            if(convertView==null){
-
-                /****** Inflate tabitem.xml file for each row ( Defined below ) *******/
-                view = inflater.inflate(R.layout.layout_currency_row, null);
-
-                /****** View Holder Object to contain tabitem.xml file elements ******/
-
-                holder = new ViewHolder();
-                holder.countryTextView = (TextView) view.findViewById(R.id.countryTextView);
-                holder.currencyTextView=(TextView)view.findViewById(R.id.currencyTextView);
-
-                /************  Set holder with LayoutInflater ************/
-                view.setTag(holder);
-            }
-            else {
-                holder = (ViewHolder) view.getTag();
-            }
-
-            Currency tempValues = currencyArrayList.get(position);
-            holder.countryTextView.setText(tempValues.country);
-            holder.currencyTextView.setText(String.format("%.3f", tempValues.currencyAmount));
-
-            return view;
-        }
-    }
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_currency_converter);
 
-        if(requestQueue == null) {
+        if (requestQueue == null) {
             requestQueue = Volley.newRequestQueue(this);
         }
 
@@ -169,9 +97,27 @@ public class CurrencyConverterActivity extends AppCompatActivity {
         usDollarsEditText.setOnEditorActionListener(new TextView.OnEditorActionListener() {
             @Override
             public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
-                Toast.makeText(CurrencyConverterActivity.this, "update currencies...", Toast.LENGTH_SHORT).show();
-                setCurrencies(Integer.parseInt(usDollarsEditText.getText().toString()));
-                return true;
+                try {
+                    Toast.makeText(CurrencyConverterActivity.this, "update currencies...", Toast.LENGTH_SHORT).show();
+                    setCurrencies(Integer.parseInt(usDollarsEditText.getText().toString()));
+                    return true;
+                } catch (Exception e) {
+                    AlertDialog alertDialog = new AlertDialog.Builder(CurrencyConverterActivity.this)
+                            .setTitle("Invalid Number").setMessage("You have entered an invalid number. Please try again.")
+                            .setPositiveButton(android.R.string.ok, new DialogInterface.OnClickListener() {
+                                @Override
+                                public void onClick(DialogInterface dialog, int which) {
+                                    usDollarsEditText.setText("");
+                                }
+                            })
+                            .setNegativeButton(android.R.string.cancel, new DialogInterface.OnClickListener() {
+                                @Override
+                                public void onClick(DialogInterface dialog, int which) {
+                                    //let user edit existing value?
+                                }
+                            }).show();
+                    return false;
+                }
             }
         });
 
@@ -204,5 +150,76 @@ public class CurrencyConverterActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public static class ViewHolder {
+        public TextView countryTextView;
+        public TextView currencyTextView;
+    }
+
+    private class Currency {
+        String country;
+        Double currencyAmount;
+
+        public Currency(String country, Double currencyAmount) {
+            this.country = country;
+            this.currencyAmount = currencyAmount;
+        }
+    }
+
+    private class CurrencyAdapter extends BaseAdapter {
+
+        private final Activity activity;
+        private final LayoutInflater inflater;
+
+        public CurrencyAdapter(Activity a) {
+            this.activity = a;
+            this.inflater = (LayoutInflater) activity.
+                    getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        }
+
+        @Override
+        public int getCount() {
+            return COUNTRY_COUNT;
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return position;
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return position;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            View view = convertView;
+            ViewHolder holder;
+
+            if (convertView == null) {
+
+                /****** Inflate tabitem.xml file for each row ( Defined below ) *******/
+                view = inflater.inflate(R.layout.layout_currency_row, null);
+
+                /****** View Holder Object to contain tabitem.xml file elements ******/
+
+                holder = new ViewHolder();
+                holder.countryTextView = (TextView) view.findViewById(R.id.countryTextView);
+                holder.currencyTextView = (TextView) view.findViewById(R.id.currencyTextView);
+
+                /************  Set holder with LayoutInflater ************/
+                view.setTag(holder);
+            } else {
+                holder = (ViewHolder) view.getTag();
+            }
+
+            Currency tempValues = currencyArrayList.get(position);
+            holder.countryTextView.setText(tempValues.country);
+            holder.currencyTextView.setText(String.format("%.3f", tempValues.currencyAmount));
+
+            return view;
+        }
     }
 }
